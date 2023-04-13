5d48404346aa10fab72c72db66a4c7486b6647f6
Tjark Gall <tjark.gall@urban-framework.com>
2023-04-13 23:48:07 +0200

    corrected errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ scen_desc = []
 for i in range(no_scen):
     default_name = f'S{i + 1}: '
     if i == 0:
-        default_name += 'Saclay 2.0'
+        default_name += '2030: Saclay 2.0'
         default_desc = 'Continuation of today’s development. The Saclay Plateau today is dominated by universities ' \
                        'and technology-related institutions. Some residential buildings and other functions ' \
                        'exist and are growing. Nevertheless, on weekend or holiday periods, ' \
@@ -52,21 +52,21 @@ for i in range(no_scen):
                        'depending on the incoming commuters, primarily between Tuesday and Thursday and ' \
                        'barely staying or utilising other functions on the plateau.'
     elif i == 1:
-        default_name += 'Paris 2.0'
+        default_name += '2030: Paris 2.0'
         default_desc = 'High-density, mixed-use neighbourhood. The second scenario is more optimistic on the ' \
                        'integrated development of the plateau. It assumes that a large number of residential' \
                        ' developments, going further than only student and international researcher housing, ' \
                        'adds a critical mass of population density to allow for a variety of other functions to ' \
                        'arise and remain active even in holiday seasons or weekends.'
     elif i == 2:
-        default_name += 'Rural Campus'
+        default_name += '2030: Rural Campus'
         default_desc = 'Low-density, low diversity rural district. This scenario describes mostly the plateau as it ' \
                        'has been since the 1970s. While more offices and universities are added, its functions and ' \
                        'character remains primarily rural. Residential functions, as well as the accompanying ' \
                        'other functions, remain limited and their growth stagnates, maintaining primarily the status ' \
                        'quo of activity and functional mix.'
     elif i == 3:
-        default_name += 'Village Campus'
+        default_name += '2030: Village Campus'
         default_desc = 'High-density active core, surrounded by low-density. As a mix between the scenario ' \
                        '‘Paris 2.0’ and ‘Rural Campus’, this scenario is defined by overall low density and ' \
                        'restricted developments. However, it has modern yet traditional French village cores with ' \
@@ -188,7 +188,7 @@ for i in range(no_pers):
 
 # Persona characteristics
 st.subheader('Persona characteristics')
-st.write('Set the number of home-work-home kilometres for a normal day for each persona and their weight in '
+st.write('Set the number of home-work-home kilometres for a normal day for each persona and their bodyweight in '
          'kilograms (for calorie calculations).')
 
 # Define default values for all personas
@@ -197,7 +197,7 @@ for i in range(4, no_pers):
     default_values[f'Persona {i + 1}'] = [0, 0]
 
 # Create the DataFrame with the default values
-pers_chars = pd.DataFrame.from_dict(default_values, orient='index', columns=['Distance (km)', 'Weight (kg)']).fillna(0)
+pers_chars = pd.DataFrame.from_dict(default_values, orient='index', columns=['Distance (km)', 'Bodyweight (kg)']).fillna(0)
 
 pers_chars = st.experimental_data_editor(pers_chars)
 
@@ -529,7 +529,7 @@ for i in range(no_scen):
     ).properties(
         width=160,
         title={
-            'text': 'Modal shift in km for ' + scen_names[i],
+            'text': 'Modal share in km for ' + scen_names[i],
             'fontSize': 16,
             'fontWeight': 'bold',
             'anchor': 'start',
@@ -1051,7 +1051,7 @@ for i in range(no_scen):
     ).properties(
         width=160,
         title={
-            'text': 'With intervention: Modal shift in km for ' + scen_names[i],
+            'text': 'With intervention: Modal share in km for ' + scen_names[i],
             'fontSize': 16,
             'fontWeight': 'bold',
             'anchor': 'start',
